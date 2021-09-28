fa1e51414b4c38d825b1b487c03254b1a16e5cd7
Alexander Kidd <alexander.t.kidd@gmail.com>
2021-09-28 11:55:56 -0400

    added new methods to movies class

--- Movies/Movies.cs
+++ Movies/Movies.cs
@@ -9,6 +9,7 @@ namespace Movies
     //create five movies in program class
     class Movies
     {
+        private static int NextId { get; set; } = 1;
         public int Id { get; set; }
         public string Title { get; set; }
         public string Genre { get; set; }
@@ -16,17 +17,34 @@ namespace Movies
         public int Released { get; set; } //Year only
         public int RuntimeInMinutes { get; set; }
         public string Director { get; set; }
+        
         public void GetRuntime(int Hours,int Minutes)
         {
             RuntimeInMinutes = Hours * 60 + Minutes;
             Console.WriteLine($"{Title} has a runtime of {RuntimeInMinutes} minutes");
         }
+        public string Print()
+        {
+            return $"{Id} | {Title} | {Genre}";
+        }
+        public static int CalcRuntimeInMinutes(int Hours, int Minutes)
+        {
+            return Hours * 60 + Minutes;
+        }
+        public string GetRating()
+        {
+            return this.Rating;
+        }
 
+        public void SetRating(string rating)
+        {
+            Rating = rating;
+        }
 
         public Movies() { }
-        public Movies(int id, string title, string genre, string rating, int released, int runtimeinminutes, string director) 
+        public Movies(string title, string genre, string rating, int released, int runtimeinminutes, string director) 
         {
-            this.Id = id;
+            this.Id = NextId++;
             this.Title = title;
             this.Genre = genre;
             this.Rating = rating;
--- Movies/Program.cs
+++ Movies/Program.cs
@@ -6,12 +6,17 @@ namespace Movies
     {
         static void Main(string[] args)
         {
-            var ROTK = new Movies(1,"Return of the King","Fantasy","PG-13",2003,0,"Peter Jackson");
+            var ROTK = new Movies("Return of the King","Fantasy","PG-13",2003,0,"Peter Jackson");
             ROTK.GetRuntime(4, 1);
-            var Fellowship = new Movies(2, "Fellowship of the Ring", "Fantasy", "PG-13", 2001, 0, "Peter Jackson");
-            var Towers = new Movies(3, "The Two Towers", "Fantasy", "PG-13", 2002, 0, "Peter Jackson");
+            var Fellowship = new Movies("Fellowship of the Ring", "Fantasy", "PG-13", 2001, 0, "Peter Jackson");
+            var Towers = new Movies("The Two Towers", "Fantasy", "PG-13", 2002, 0, "Peter Jackson");
             Fellowship.GetRuntime(2, 58);
             Towers.GetRuntime(2, 59);
+
+            Console.WriteLine(ROTK.Print());
+            Console.WriteLine(Fellowship.Print());
+            Console.WriteLine(Towers.Print());
+
         }
     }
 }
