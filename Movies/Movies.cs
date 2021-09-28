82699ac2eeedcbe1397a2de42bbb33585fca9675
Alexander Kidd <alexander.t.kidd@gmail.com>
2021-09-28 10:15:30 -0400

    added GetRuntime method

--- Movies/Movies.cs
+++ Movies/Movies.cs
@@ -5,16 +5,23 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Movies
-{
+{ // add to class a method to set the runtime to minutes that takes two parameters, hours and minutes
+    //create five movies in program class
     class Movies
     {
         public int Id { get; set; }
         public string Title { get; set; }
         public string Genre { get; set; }
-        public string Rating { get; set; }
+        public string Rating { get; set; } = "PG";
         public int Released { get; set; } //Year only
         public int RuntimeInMinutes { get; set; }
         public string Director { get; set; }
+        public void GetRuntime(int Hours,int Minutes)
+        {
+            RuntimeInMinutes = Hours * 60 + Minutes;
+            Console.WriteLine($"{Title} has a runtime of {RuntimeInMinutes} minutes");
+        }
+
 
         public Movies() { }
         public Movies(int id, string title, string genre, string rating, int released, int runtimeinminutes, string director) 
--- Movies/Program.cs
+++ Movies/Program.cs
@@ -6,7 +6,12 @@ namespace Movies
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var ROTK = new Movies(1,"Return of the King","Fantasy","PG-13",2003,0,"Peter Jackson");
+            ROTK.GetRuntime(4, 1);
+            var Fellowship = new Movies(2, "Fellowship of the Ring", "Fantasy", "PG-13", 2001, 0, "Peter Jackson");
+            var Towers = new Movies(3, "The Two Towers", "Fantasy", "PG-13", 2002, 0, "Peter Jackson");
+            Fellowship.GetRuntime(2, 58);
+            Towers.GetRuntime(2, 59);
         }
     }
 }
